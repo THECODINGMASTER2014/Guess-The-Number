dc513981c0be3dcb37d35cddf0275aca6d6f1e87
THECODINGMASTER2014 <talwarmoksh@gmail.com>
2026-03-22 11:46:17 +0530

    a

---
+++ Index.py
@@ -0,0 +1,33 @@
+import random
+on = random.randint(0,100)
+if on % 2 == 0:
+    print("The Number to guess is Even")
+else :
+    print("The Number to guess is Odd")
+
+c = 1
+
+while c < 7 :
+ gn = int(input("Guess :"))
+ c += 1
+ if on == gn :
+  print("Correct Guess. Congratulations!🎉🎉👏🏻")
+ elif on > gn :
+    print("The Guess is Too Small")
+ elif gn > on :
+    print("The Guess is Too High ")
+ if  c == 6 :
+    print("Sorry. There are only 6 chances and You finished them All. You Lost 😞😓.")
+
+
+   
+
+
+
+
+
+
+
+
+     
+
