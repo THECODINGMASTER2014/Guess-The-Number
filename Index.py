e15830b20a75c8f973850697d3a7cf632a7ca47b
THECODINGMASTER2014 <talwarmoksh@gmail.com>
2026-03-23 21:09:32 +0530

    a

--- Index.py
+++ Index.py
@@ -12,13 +12,13 @@ while c < 7 :
  gn = int(input("Guess :"))
  c += 1
  if on == gn :
-  print("Correct Guess. Congratulations!🎉🎉👏🏻")
+  print("Correct Guess. Congratulations!🎉🎉👏🏻👏🏻")
  elif on > gn :
     print("The Guess is Too Small")
  elif gn > on :
     print("The Guess is Too High ")
  if c == 7 and gn != on  :
-    print("Sorry. There are only 6 chances and You finished them All. You Lost 😞😓.")
+    print("Sorry. There are only 6 chances and You finished them All. You Lost 😒😫😞😓.")
 
 
    
